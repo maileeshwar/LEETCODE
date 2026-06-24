5024b49fcb8e59f1d6f9f5ecf4673a2e2cc47678
Maileeshwar <maileeshsrinivasan@gmail.com>
2026-06-24 11:26:18 +0530

    Time: 36 ms (90.48%), Space: 24.3 MB (90.48%) - LeetHub

---
+++ 3523-make-array-non-decreasing/3523-make-array-non-decreasing.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maximumPossibleSize(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = b =0
+        for i in nums:
+            if i>=b:
+                b=i
+                a+=1
+        return a
