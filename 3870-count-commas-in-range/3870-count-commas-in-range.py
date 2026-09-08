b80686c82a4c603d86c63b8b6def68b476b23962
Maileeshwar <maileeshsrinivasan@gmail.com>
2026-09-08 15:17:11 +0530

    Time: 255 ms (15.53%), Space: 15.9 MB (5.83%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c=0
+        for i in range(1000,n+1):
+            c+=1
+        return c
+        
